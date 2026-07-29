fad25e30700422f085b25f3c8bd27282202c48ca
SeesrithaChowdary <seesrithachowdary@gmail.com>
2026-07-30 00:16:20 +0530

    Delete Week-6-Angular directory

--- Week-6-Angular/README.md
+++
@@ -1,3 +0,0 @@
-# Week-6-Angular
-
-Angular Week 6 exercises: student course portal, routing modules, pipes, custom directives, and guards.
--- Week-6-Angular/student-course-portal/src/app/features/enrollment/reactive-enrollment-form/reactive-enrollment-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-reactive-enrollment-form',
-  imports: [],
-  templateUrl: './reactive-enrollment-form.html',
-  styleUrl: './reactive-enrollment-form.css',
-})
-export class ReactiveEnrollmentForm {}
